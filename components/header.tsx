48a76bb89bdf653858436d74a9672358274bae49
Leonard Roussard <lroussard@gmail.com>
2025-04-08 23:42:17 +0200

    refactor: header

--- components/header.tsx
+++ components/header.tsx
@@ -1,28 +1,29 @@
-import Link from 'next/link';
-import dynamic from 'next/dynamic';
+"use client";
 
-const DynamicThemeSwitch = dynamic(() => import('./theme-switch'), {
-  ssr: false,
-});
+import { PROJECT } from "@/constants/project";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import { Logo } from "./logo";
+
+const ThemeSwitchWrapper = () => {
+  const DynamicThemeSwitch = dynamic(() => import("./theme-switch"), {
+    ssr: false,
+  });
+  return <DynamicThemeSwitch />;
+};
 
 export default function Header() {
   return (
-    <div className='border-b border-stroke-soft-200'>
-      <header className='mx-auto flex h-14 max-w-5xl items-center justify-between px-5'>
+    <div className="border-b border-stroke-soft-200">
+      <header className="mx-auto flex h-14 max-w-5xl items-center justify-between px-5">
         <Link
-          href='/'
-          className='flex items-center gap-2 text-label-md text-text-strong-950'
+          href="/"
+          className="flex items-center gap-2 text-label-md text-text-strong-950 font-mono"
         >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src='/images/logo.svg'
-            alt=''
-            className='size-9 object-contain'
-          />
-          AlignUI
+          <Logo className="size-7" />
+          {PROJECT.NAME}
         </Link>
-
-        <DynamicThemeSwitch />
+        <ThemeSwitchWrapper />
       </header>
     </div>
   );
